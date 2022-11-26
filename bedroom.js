3596e4acd00e326a66f806ff2be4e6fab82c8f5a
Quesz <98159675+WVaidik@users.noreply.github.com>
2022-11-26 17:51:38 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,54 @@
+img = "";
+status = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(640, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+
+function preload() {
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+ if (error) {
+    console.log(error);
+ }
+ console.log(results);
+ objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    if(status != "")
+    {
+      r = random(255);
+      g = random(255);
+      b = random(255);
+      objectDetector.detect(video, gotResult);
+      for (i = 0; i < objects.length; i++)
+      {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+
+        fill(r, g, b);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        stroke(r, g, b);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
+}
